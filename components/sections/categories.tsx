a62e38b985ba84407d88127750e4eb79dadaf340
Mullajonov Muhsinjon <122345056+al-muhsinjon@users.noreply.github.com>
2023-10-06 13:24:12 +0500

    Finish all

--- components/sections/categories.tsx
+++ components/sections/categories.tsx
@@ -7,7 +7,7 @@ const Categories = () => {
   return (
     <div className="mt-12 p-3">
       <Container>
-        <div className="grid grid-cols-2 gap-5 items-center">
+        <div className="grid grid-cols-2 gap-5 items-center text-white">
           {categories.map((category) => (
             <CategoryItems key={category.id} category={category} />
           ))}
